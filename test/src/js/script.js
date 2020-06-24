bf62195f636f0e594732f2066cc5762ab206849e
ReCen21 <alpha55757@gmail.com>
2020-06-24 21:37:43 +0300

    complete 1-st exercise

---
+++ test/src/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/* alert("Hello World!");
+console.log( confirm("Are you here?") ); */
+
+const answer = +prompt("Are you older than eighteen?", "");
+alert(answer);
+alert( typeof(answer) );
